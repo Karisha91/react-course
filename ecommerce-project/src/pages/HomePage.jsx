401ca2c3cb026606ade194b36d97e7346066e593
Karisha91 <ivan.djurdjevic991@gmail.com>
2025-08-13 13:12:00 +0200

    fetch data from database

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,7 +2,20 @@ import Header from '../components/Header';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 import { products } from '../../starting-code/data/products'; 
+import { useEffect } from 'react';
+
 function HomePage() {
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/products').then((response) => {
+      response.json().then((data) => {
+        // Assuming the API returns an array of products
+        // You can set the products state here if you have a state management solution
+        console.log(data);
+      });
+    });
+  }, []);
+
   return (
     <>
     <title>Ecommerce Project</title>
@@ -68,52 +81,6 @@ function HomePage() {
         })}
         
 
-          <div className="product-image-container">
-            <img className="product-image"
-              src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
-          </div>
-
-          <div className="product-name limit-text-to-2-lines">
-            Adults Plain Cotton T-Shirt - 2 Pack
-          </div>
-
-          <div className="product-rating-container">
-            <img className="product-rating-stars"
-              src="images/ratings/rating-45.png" />
-            <div className="product-rating-count link-primary">
-              56
-            </div>
-          </div>
-
-          <div className="product-price">
-            $7.99
-          </div>
-
-          <div className="product-quantity-container">
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div className="product-spacer"></div>
-
-          <div className="added-to-cart">
-            <img src={CheckmarkIcon} />
-            Added
-          </div>
-
-          <button className="add-to-cart-button button-primary">
-            Add to Cart
-          </button>
         </div>
       </div>
     </>
